5257b742cdd3be75d7aa107621d666e30a1bf05b
Teacher <987262086@qq.com>
2018-07-09 14:05:02 +0800

    将创建集合代码放到方法内部

--- src/com/shujia/read/mainDemo.java
+++ src/com/shujia/read/mainDemo.java
@@ -14,44 +14,34 @@ import java.util.ArrayList;
  */
 public class mainDemo {
     public static void main(String[] args) throws Exception {
-        ArrayList<Score> scores = new ArrayList<>();
-
         //读取分数表
-        readScore(scores);
+        ArrayList<Score> scores = readScore();
 
         for (Score score : scores) {
             //System.out.println(score);
         }
-
-        ArrayList<Student> students = new ArrayList<>();
-
         //读取学生表
-        readStudent(students);
+        ArrayList<Student> students  = readStudent();
 
         for (Student student : students) {
             //System.out.println(student);
         }
 
-        ArrayList<Cource> cources = new ArrayList<>();
         //读取科目表
-        readCource(cources);
+        ArrayList<Cource> cources = readCource();
 
         for (Cource cource : cources) {
             System.out.println(cource);
         }
-
-
-
     }
-
 
     /**
      *读取分数表
      *
-     * @param scores 存放分数的集合
      * @throws Exception
      */
-    public static void readScore(ArrayList<Score> scores) throws Exception {
+    public static  ArrayList<Score> readScore() throws Exception {
+        ArrayList<Score> scores = new ArrayList<>();
         //相对路径读取文件
         FileInputStream fileInputStream = new FileInputStream("data/score.txt");
         //字节流转字符流可以指定转换编码
@@ -67,16 +57,17 @@ public class mainDemo {
             score.setScore(Integer.parseInt(strs[2]));
             scores.add(score);
         }
+        return scores;
     }
 
     /**
      *读取学生表
      *
-     * @param students
      * @throws Exception
      */
 
-    public static void readStudent(ArrayList<Student> students) throws Exception{
+    public static  ArrayList<Student> readStudent() throws Exception{
+        ArrayList<Student> students = new ArrayList<>();
         //相对路径读取文件
         FileInputStream fileInputStream = new FileInputStream("data/students.txt");
         //字节流转字符流可以指定转换编码
@@ -88,10 +79,12 @@ public class mainDemo {
             Student student = new Student(strs[0],strs[1],Integer.parseInt(strs[2]),strs[3],strs[4]);
             students.add(student);
         }
+        return students;
     }
 
 
-    public static void readCource(ArrayList<Cource> cources)throws Exception{
+    public static  ArrayList<Cource> readCource()throws Exception{
+        ArrayList<Cource> cources = new ArrayList<>();
         //相对路径读取文件
         FileInputStream fileInputStream = new FileInputStream("data/Cource.txt");
         //字节流转字符流可以指定转换编码
@@ -105,5 +98,6 @@ public class mainDemo {
 
             cources.add(cource);
         }
+        return cources;
     }
 }
